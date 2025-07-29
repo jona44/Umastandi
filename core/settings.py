93eedd7f7fd4af1856f077a50bdf8d9591d77c62
jona44 <tjmanyadza@live.com>
2025-07-29 13:15:09 +0200

    ALLOWED HOST

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY =os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False") == "True"
 
-ALLOWED_HOSTS = os.environ.get ("ALLOWED_HOST").split(" ")
+ALLOWED_HOSTS = [host for host in os.environ.get("ALLOWED_HOST", "").split(" ") if host]
 
 
 # Application definition
