1271795de96504ce315c56ed6e9a5355830bec54
jona44 <tjmanyadza@live.com>
2025-07-30 11:26:30 +0200

    wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -8,12 +8,9 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 
-# Use DJANGO_PRODUCTION=true to activate production settings
-if os.getenv('DJANGO_PRODUCTION', '').lower() == 'true':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.production')
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')  # or just 'core.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
